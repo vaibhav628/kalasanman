4b582589dc42b52e43045a3b98536a501b6c3dfc
vaibhav628 <vaibhav_d@yahoo.com>
2019-11-02 20:35:35 -0400

    membership form created

--- src/app/membership/membership.module.ts
+++ src/app/membership/membership.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -20,6 +19,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes)
   ],
   declarations: [MembershipPage]
--- src/app/membership/membership.page.ts
+++ src/app/membership/membership.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, ModalController } from '@ionic/angular';
 import { AuthenticateService } from '../services/authentication.service';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-membership',
@@ -14,17 +15,76 @@ export class MembershipPage implements OnInit {
 
   constructor(
     private navCtrl: NavController,
-    private authService: AuthenticateService
+    private authService: AuthenticateService,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit(){
+
+    validations_form: FormGroup;
 
     if(this.authService.userDetails()){
       this.userEmail = this.authService.userDetails().email;
     }else{
       this.navCtrl.navigateBack('');
     }
+
+        this.validations_form = this.formBuilder.group({
+        email: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+        ])),
+        password: new FormControl('', Validators.compose([
+          Validators.minLength(5),
+          Validators.required
+        ])),
+         fname: new FormControl('', Validators.compose([
+                  Validators.minLength(3),
+                  Validators.required,
+                  Validators.pattern('^[a-zA-Z+-]+$')
+         ])),
+         lname: new FormControl('', Validators.compose([
+                  Validators.minLength(3),
+                  Validators.required,
+                  Validators.pattern('^[a-zA-Z+-]+$')
+         ])),
+         cnumber: new FormControl('', Validators.compose([
+                           Validators.minLength(10),
+                           Validators.maxLength(10),
+                           Validators.required,
+                           Validators.pattern('^[0-9+-]+$')
+                  ])),
+      });
+
   }
+
+  validation_messages = {
+      'fname': [
+          { type: 'required', message: 'First name is required.' },
+          { type: 'minlength', message: 'First Name must be at least 3 characters long.' },
+          { type: 'pattern', message: 'Enter valid first name' },
+        ],
+      'lname': [
+                { type: 'required', message: 'Last name is required.' },
+                { type: 'minlength', message: 'Last Name must be at least 3 characters long.' },
+                { type: 'pattern', message: 'Enter valid last name' },
+        ],
+      'cnumber': [
+                      { type: 'required', message: 'Contact number is required.' },
+                      { type: 'minlength', message: 'Enter phone number with area code' },
+                      { type: 'maxlength', message: 'Enter phone number with area code' },
+                      { type: 'pattern', message: 'Enter valid contact number' },
+              ],
+
+      'email': [
+              { type: 'required', message: 'Email is required.' },
+              { type: 'pattern', message: 'Please enter a valid email.' }
+            ],
+            'password': [
+              { type: 'required', message: 'Password is required.' },
+              { type: 'minlength', message: 'Password must be at least 5 characters long.' }
+            ]
+  };
 
   logout(){
     this.authService.logoutUser()
