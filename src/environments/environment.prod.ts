ed6007cfcb3b839d0656ea02fb04cc0c649c90a0
Vaibhav628 <vaibhav_d@yahoo.com>
2019-11-30 15:15:25 -0500

    Prod environment updated with firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+      apiKey: "AIzaSyAKHIwm7_jTgEurBRBKWTVnlVzuefcMrsY",
+      authDomain: "kalasanman-e68fa.firebaseapp.com",
+      databaseURL: "https://kalasanman-e68fa.firebaseio.com",
+      projectId: "kalasanman-e68fa",
+      storageBucket: "kalasanman-e68fa.appspot.com",
+      messagingSenderId: "425876689460",
+      appId: "1:425876689460:web:7ad8aefe20c969bfe0b061",
+      measurementId: "G-KSSMJKFW3N"
+     }
 };
